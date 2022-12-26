f8429fb446cd0086c72e2ed8b23cef3a9ad6954a
snociloL <aqil.radzi05@gmail.com>
2022-12-26 19:30:55 +0800

    Authentication feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,35 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'User created and logged in');
     }
+
+    // Logout User
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out successfully.');
+    }
+
+    // Show Login form
+    public function login() {
+        return view('users.login');
+    }
+
+        // Show Authenticate User
+        public function authenticate(Request $request) {
+            $formFields = $request->validate([
+                'email' => ['required', 'email'],
+                'password' => 'required'
+            ]);
+
+            if(auth()->attempt($formFields)) {
+                $request->session()->regenerate();
+
+                return redirect('/')->with('message', 'You are now logged in!');
+            }
+
+            return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+        }
 }
