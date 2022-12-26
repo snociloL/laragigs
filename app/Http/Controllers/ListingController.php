83d8fe0a563650682eb3a3d8b71921d8e7594e44
snociloL <aqil.radzi05@gmail.com>
2022-12-26 16:33:55 +0800

    Delete Button

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -43,4 +43,36 @@ class ListingController extends Controller
         Listing::create($formFields);
         return redirect('/')->with('message', 'Listing created successfully!');
     }
+
+    // Show Edit Form
+    public function edit(Listing $listing) {
+        return view('listings.edit', ['listing' =>$listing]);
+    }
+
+    // Update Listing Data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+        return back()->with('message', 'Listing updated successfully!');
+    }
+
+    // Delete Listing
+
+    public function destroy(Listing $listing) {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully');
+    }
 }
